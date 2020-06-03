31b9031f67c0ff405d1c6122675e7a79f4d3dad2
Krystian Otto <otto.krystiann@gmail.com>
2020-06-03 13:19:13 +0200

    add func playGame and EventListener for buttons

--- js/script.js
+++ js/script.js
@@ -1,38 +1,44 @@
-function getMoveName(moveId){
-    if (moveId == 1){
-        return 'rock'
-    }else if(moveId == 2){
-        return 'paper'
-    }else if (moveId == 3){
-        return 'scissors'
-    }else{
-        alert('You picked the wrong button sir')
-    }
-}
+function playGame(playerInput){
+    
+    clearMessages()
 
-function displayResult(computerMove, playerMove){
-    printMessage('You play ' + playerMove + ',' + 'computer plays ' + computerMove)
-    if (playerMove == computerMove){
-        printMessage('Draw !')
-    }else if(playerMove == 'rock' && computerMove == 'paper'){
-        printMessage('You lose')
-    }else if(playerMove == 'rock' && computerMove == 'scissors'){
-        printMessage('You win')
-    }else if(playerMove == 'scissors' && computerMove == 'paper'){
-        printMessage('You win')
-    }else if(playerMove == 'scissors' && computerMove == 'rock'){
-        printMessage('You lose')
-    }else if(playerMove == 'paper' && computerMove == 'scissors'){
-        printMessage('You lose')
-    }else if(playerMove == 'paper' && computerMove == 'rock'){
-        printMessage('You win')
-    }
-}
+    let playerMove = getMoveName(playerInput);
+    
+    let randomNumber = Math.floor(Math.random() *3 +1);
+    let computerMove = getMoveName(randomNumber);
 
-let randomNumber = Math.floor(Math.random() *3 +1);
-let computerMove = getMoveName(randomNumber);
+    function getMoveName(moveId){
+        if (moveId == 1){
+            return 'rock'
+        }else if(moveId == 2){
+            return 'paper'
+        }else{
+            return 'scissors'
+        }
+    };
 
-let playerInput = prompt('Choose your Destiny! 1: rock 2: paper 3: scissors');
-let playerMove = getMoveName(playerInput)
+    function displayResult(computerMove, playerMove){
+        printMessage('You play ' + playerMove + ',' + 'computer plays ' + computerMove)
+        if (playerMove == computerMove){
+            printMessage('Draw !')
+        }else if(playerMove == 'rock' && computerMove == 'paper'){
+            printMessage('You lose')
+        }else if(playerMove == 'rock' && computerMove == 'scissors'){
+            printMessage('You win')
+        }else if(playerMove == 'scissors' && computerMove == 'paper'){
+            printMessage('You win')
+        }else if(playerMove == 'scissors' && computerMove == 'rock'){
+            printMessage('You lose')
+        }else if(playerMove == 'paper' && computerMove == 'scissors'){
+            printMessage('You lose')
+        }else if(playerMove == 'paper' && computerMove == 'rock'){
+            printMessage('You win')
+        }
+    };
 
-displayResult(computerMove, playerMove)
+    displayResult(computerMove, playerMove);
+};
+
+document.getElementById('btn-rock').addEventListener('click',function(){playGame(1)});
+document.getElementById('btn-paper').addEventListener('click',function(){playGame(1)});
+document.getElementById('btn-scissors').addEventListener('click',function(){playGame(1)});
