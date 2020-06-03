d1393ada127d1de85ee39f1376a3f56480b405ca
Krystian Otto <otto.krystiann@gmail.com>
2020-06-03 11:59:44 +0200

    add if's to compare players moves

--- js/script.js
+++ js/script.js
@@ -23,6 +23,19 @@ if(playerInput == 1){
     alert('You picked the wrong button sir')
 }
 
-printMessage('Computers Move is ' + computerMove);
 
-printMessage('Your Move is '+ playerMove);
+if (playerMove == computerMove){
+    printMessage('Draw !')
+}else if(playerMove == 'rock' && computerMove == 'paper'){
+    printMessage('You lose')
+}else if(playerMove == 'rock' && computerMove == 'scissors'){
+    printMessage('You win')
+}else if(playerMove == 'scissors' && computerMove == 'paper'){
+    printMessage('You win')
+}else if(playerMove == 'scissors' && computerMove == 'rock'){
+    printMessage('You lose')
+}else if(playerMove == 'paper' && computerMove == 'scissors'){
+    printMessage('You lose')
+}else if(playerMove == 'paper' && computerMove == 'rock'){
+    printMessage('You win')
+}
