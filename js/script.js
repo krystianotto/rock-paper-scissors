0c3afd2ff69407f1927b6ef368c6dd541d5bb0d6
Krystian Otto <otto.krystiann@gmail.com>
2020-06-03 11:47:53 +0200

    add playerMove

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-let computerMove = 'Unknown move';
+let playerInput = prompt('Choose your Destiny! 1: rock 2: paper 3: scissors');
 
+let computerMove = 'Unknown move';
 let playerMove = 'Unknown move';
 
 let randomNumber = Math.floor(Math.random() *3 +1);
 
 if(randomNumber == 1){
-    computerMove = 'kamień';
+    computerMove = 'rock';
 }else if(randomNumber == 2){
-    computerMove = 'papier';
+    computerMove = 'paper';
 }else{
-    computerMove = 'nożyce';
+    computerMove =  'scissors';
 }
 
-printMessage('Wylosowana wartość to ' + computerMove)
+if(playerInput == 1){
+    playerMove = 'rock';
+}else if(playerInput == 2){
+    playerMove = 'paper'
+}else if(playerInput == 3){
+    playerMove = 'scissors'
+}else{
+    alert('You picked the wrong button sir')
+}
+
+printMessage('Computers Move is ' + computerMove);
+
+printMessage('Your Move is '+ playerMove);
