b614469c3b1770355249eb3fd4ece85af3310680
Krystian Otto <otto.krystiann@gmail.com>
2020-06-04 20:36:00 +0200

    add round counter and lose/win game func

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
 function playGame(playerInput){
-    
-    clearMessages()
 
     const playerMove = getMoveName(playerInput);
-    
     const randomNumber = Math.floor(Math.random() * 3 + 1);
     const computerMove = getMoveName(randomNumber);
-    console.log(computerMove)
 
     function getMoveName(moveId){
-        // moveId === 1 ? 'rock' : moveId === 2 ? 'paper' : 'scissors';
         if (moveId === 1){
             return 'rock'
         }else if(moveId === 2){
@@ -19,20 +14,50 @@ function playGame(playerInput){
         }
     };
 
-    function displayResult(computerMove, playerMove){
-        printMessage('You play ' + playerMove + ',' + 'computer plays ' + computerMove)
+    function gameResult(computerMove, playerMove){
+        //printMessage('You play ' + playerMove + ',' + 'computer plays ' + computerMove);
         if ((playerMove === 'rock' && computerMove === 'scissors') || (playerMove === 'scissors' && computerMove === 'paper') || (playerMove === 'paper' && computerMove === 'rock')){
-            printMessage('You win !')
+            winGame();
         }else if((playerMove === 'rock' && computerMove === 'paper') || (playerMove === 'scissors' && computerMove === 'rock') || (playerMove === 'paper' && computerMove === 'scissors')){
-            printMessage('You lose')
+            loseGame();
         }else{
-            printMessage('Draw !')
+            console.log('Draw !')
         };
     };
 
-    displayResult(computerMove, playerMove);
+    function winGame(){
+        playerScore++;
+        playerScore_span.innerHTML = playerScore;
+        computerScore_span.innerHTML = computerScore;
+        checkScore(playerScore, computerScore)
+    }
+
+    function loseGame(){
+        computerScore++;
+        computerScore_span.innerHTML = computerScore;
+        playerScore_span.innerHTML = playerScore;
+        checkScore(playerScore, computerScore)
+    }
+
+    function checkScore(playerScore, computerScore){
+        (computerScore == roundToPlay) || (playerScore == roundToPlay) ? confirm('Game Over') : false;
+    }
+
+    gameResult(computerMove, playerMove);
 };
 
-document.getElementById('btn-rock').addEventListener('click',function(){playGame(1)});
-document.getElementById('btn-paper').addEventListener('click',function(){playGame(2)});
-document.getElementById('btn-scissors').addEventListener('click',function(){playGame(3)});
+const roundToPlay = prompt('What score should end the game ? Put a number Down there','0')
+const playerScore_span = document.getElementById('player-score');
+const computerScore_span = document.getElementById('computer-score');
+let playerScore = 0;
+let computerScore = 0;
+
+document.getElementById('btn-rock').addEventListener('click',function(){
+    playGame(1)
+});
+document.getElementById('btn-paper').addEventListener('click',function(){
+    playGame(2)
+});
+document.getElementById('btn-scissors').addEventListener('click',function(){
+    playGame(3)
+});
